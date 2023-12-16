feafe529b9fcdc763ce3f97e68de5f31f5b92912
george <7jagjag@gmail.com>
2023-12-16 01:11:13 +0100

    is it an app now

--- src/EventList.tsx
+++ src/EventList.tsx
@@ -1,4 +1,4 @@
-import { Flex, Button, Skeleton, Text, Spacer } from '@chakra-ui/react'
+import { Flex, Button, Skeleton, Text, Spacer, Progress } from '@chakra-ui/react'
 import { SmallAddIcon, ViewIcon, EmailIcon } from '@chakra-ui/icons'
 import { Link } from '@tanstack/react-router'
 import { useQuery } from '@tanstack/react-query'
@@ -49,7 +49,15 @@ export default function EventList() {
   }
 
   return (
-    <Flex flex="1" gap="2" p="4" overflow="auto" flexDir="column">
+    <Flex flex="1" gap="2" p="4" overflow="auto" flexDir="column" position="relative">
+      {isLoading && (
+        <Flex flexDir="column" alignItems="center" justifyContent="center" zIndex="10" bg="white" position="fixed" top="0" left="0" width="100%" height="100%">
+          <Flex fontSize="6xl">Events</Flex>
+          <Flex display="block" bg="blue.400" w="200px">
+            <Progress size="xs" colorScheme="purple" isIndeterminate />
+          </Flex>
+        </Flex>
+      )}
       <Flex gap="2" justifyContent="space-around">
         <Button variant="outline" onClick={() => setFilter(filter === 'pending' ? null : 'pending')}>
           {filter === 'pending' ? 'show all' : 'show pending'}
