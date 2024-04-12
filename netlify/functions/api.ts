939aef7725fbe2ccb4e1a9df417a749debb15508
george <7jagjag@gmail.com>
2024-04-13 00:42:23 +0200

    json

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,6 +1,6 @@
 import type { Context } from "@netlify/functions"
 
 export default async (req: Request, context: Context) => {
-  console.log(req.method, req.body, context.ip)
+  console.log(req.method, req.json(), context.ip)
   return new Response(JSON.stringify({ message: "hello wolrd" }))
 }
