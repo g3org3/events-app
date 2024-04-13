ada85aa8bb40e0def61127728015efa53f997e78
george <7jagjag@gmail.com>
2024-04-13 18:04:10 +0200

    notify

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -2,6 +2,15 @@
 async function handler(event, context) {
   console.log('[INFO]', event.httpMethod, event.body)
 
+  if (event.httpMethod == 'POST') {
+    const [id, title] = event.body.split(':')
+
+    fetch(`https://ntfy.sh/events-${id}?title=Events`, {
+      method: 'PUT',
+      body: title
+    })
+  }
+
   return {
     statusCode: 200,
     body: JSON.stringify({ message: 'hello world' })
