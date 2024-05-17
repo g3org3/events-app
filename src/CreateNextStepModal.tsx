028a8f9cb86fd653f1804c2ed49ba8c4e62f8d78
george <7jagjag@gmail.com>
2024-05-18 01:08:45 +0200

    backup

--- src/CreateNextStepModal.tsx
+++ src/CreateNextStepModal.tsx
@@ -9,31 +9,40 @@ import {
   Input,
   ModalFooter,
   useDisclosure,
-} from '@chakra-ui/react'
-import { useMutation } from '@tanstack/react-query'
-import { useParams } from '@tanstack/react-router'
-import { useState } from "react"
+} from "@chakra-ui/react";
+import { useMutation } from "@tanstack/react-query";
+import { useParams } from "@tanstack/react-router";
+import { useState } from "react";
 
-import { createNextStep } from './pb'
-import { queryClient } from './queryClient'
+import { createNextStep } from "./pb";
+import { queryClient } from "./queryClient";
 
 export default function CreateNextStepModal() {
-  const { id: selectedEventId } = useParams({ from: '/event/$id/next-steps' })
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const [title, setTitle] = useState('')
+  const { id: selectedEventId } = useParams({ from: "/event/$id/next-steps" });
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [title, setTitle] = useState("");
 
   const { isPending, mutate } = useMutation({
-    mutationFn: () => createNextStep({ title, eventId: selectedEventId }, 'nextstep'),
+    mutationFn: () =>
+      createNextStep({ title, eventId: selectedEventId }, "nextstep"),
     onSuccess() {
-      queryClient.invalidateQueries({ queryKey: ['get-next-steps', `event-id-${selectedEventId}`] })
-      setTitle('')
-      onClose()
-    }
-  })
+      queryClient.invalidateQueries({
+        queryKey: ["get-next-steps", `event-id-${selectedEventId}`],
+      });
+      setTitle("");
+      onClose();
+    },
+  });
 
   return (
     <>
-      <Button flexShrink="0" colorScheme="purple" size="md" variant="outline" onClick={onOpen}>
+      <Button
+        flexShrink="0"
+        colorScheme="purple"
+        size="md"
+        variant="outline"
+        onClick={onOpen}
+      >
         Add a next step
       </Button>
       <Modal isOpen={isOpen} onClose={onClose}>
@@ -52,12 +61,17 @@ export default function CreateNextStepModal() {
             />
           </ModalBody>
           <ModalFooter>
-            <Button isLoading={isPending} colorScheme="purple" mr={3} onClick={() => mutate()}>
+            <Button
+              isLoading={isPending}
+              colorScheme="purple"
+              mr={3}
+              onClick={() => mutate()}
+            >
               add
             </Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
     </>
-  )
+  );
 }
--- src/Login.tsx
+++ src/Login.tsx
@@ -1,30 +1,39 @@
-import { Flex, Button } from '@chakra-ui/react'
-import type { ClientResponseError } from 'pocketbase'
-import { pb } from './pb'
+import { Flex, Button } from "@chakra-ui/react";
+import type { ClientResponseError } from "pocketbase";
+import { pb } from "./pb";
 
 export default function Login() {
   const onLogin = async () => {
-    let res = null
+    let res = null;
     try {
-      res = await pb.collection('users').authWithOAuth2({ provider: 'google' })
+      res = await pb.collection("users").authWithOAuth2({ provider: "google" });
     } catch (e) {
-      const err = e as ClientResponseError
-      alert(err.message)
+      const err = e as ClientResponseError;
+      alert(err.message);
     }
     if (res?.meta?.avatarUrl) {
-      const { avatarUrl } = res.meta
-      await pb.collection('users').update(res.record.id, { avatarUrl })
+      const { avatarUrl } = res.meta;
+      await pb.collection("users").update(res.record.id, { avatarUrl });
     }
-    document.location = '/'
-  }
+    document.location = "/";
+  };
 
   return (
     <Flex h="100dvh" alignItems="center" justifyContent="center" bg="gray.100">
-      <Flex p="6" w="300px" bg="gray.50" boxShadow="md" border="1px solid" borderColor="gray.100" flexDir="column" gap="4">
+      <Flex
+        p="6"
+        w="300px"
+        bg="gray.50"
+        boxShadow="md"
+        border="1px solid"
+        borderColor="gray.100"
+        flexDir="column"
+        gap="4"
+      >
         <Flex fontSize="xx-large">Login</Flex>
         <Flex>login to use events</Flex>
         <Button onClick={onLogin}>login with google</Button>
       </Flex>
     </Flex>
-  )
+  );
 }
